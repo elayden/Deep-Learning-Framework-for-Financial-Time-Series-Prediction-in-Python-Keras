9804cb752da2c1bab737f23f75e2cf1ed8044501
Elliot Layden <17534198+elayden@users.noreply.github.com>
2019-01-28 01:24:07 -0600

    Update train_stock_mdl.py

--- train_stock_mdl.py
+++ train_stock_mdl.py
@@ -2,6 +2,7 @@
 """
 train_stock_mdl.py
 Created on Wed Oct 17, 2018
+Updated on Sun Jan 27, 2019
 @author: Elliot Layden
 
 Info: this script imports a csv file containing predictors and an outcome 
@@ -102,15 +103,15 @@ def partitionData(x, y, segmentSize, trainPercent, devPercent):
 # Scale train and test data to [-1, 1] (note: uses scaler obtained from 
 #   training data to rescale dev and test sets; this is important so as to not
 #   introduce information from dev and test to training)
-def rescaleData(train, dev, test):
+def rescaleData(train, dev, test, scaleColumns):
     scaler = MinMaxScaler(feature_range=(-1, 1))
-    scaler = scaler.fit(train)
+    scaler = scaler.fit(train[:,scaleColumns])
     train = train.reshape(train.shape[0], train.shape[1])
-    trainScaled = scaler.transform(train)
+    trainScaled = np.hstack((train[:, dummyVars], scaler.transform(train[:,scaleColumns])))
     dev = dev.reshape(dev.shape[0], dev.shape[1])
-    devScaled = scaler.transform(dev)
+    devScaled = np.hstack((dev[:,dummyVars], scaler.transform(dev[:,scaleColumns])))
     test = test.reshape(test.shape[0], test.shape[1])
-    testScaled = scaler.transform(test)
+    testScaled = np.hstack((test[:,dummyVars], scaler.transform(test[:,scaleColumns])))
     return scaler, trainScaled, devScaled, testScaled
 
 def separateXY(data):
@@ -119,12 +120,12 @@ def separateXY(data):
     yUse = yUse.reshape((yUse.shape[0], 1))
     return xUse, yUse
 
-def scaleToday(today, scaler):
+def scaleToday(today, scaler, scaleColumns):
     holdY = today[-1,-1].reshape((1,1))
     today = today[:,:-1].flatten()
     today = today.reshape((1,today.shape[0]))
     today = np.hstack((today, holdY))
-    today_scaled = scaler.transform(today)
+    today_scaled = np.hstack((today[:,dummyVars], scaler.transform(today[:,scaleColumns])))
     today_scaled = today_scaled[:,:-1]
     return today_scaled
 
@@ -165,6 +166,8 @@ def calcPerformance(yReal, yHat):
     rmse_null = np.sqrt(((yReal - np.mean(yReal)) ** 2).mean(axis=0)) * 100
     rmseImprove = (rmse_null-rmse)/rmse_null * 100
     
+    rsquared = np.corrcoef(np.transpose(yReal), np.transpose(yHat))[0,1]**2
+    
     # Annualized Return (https://www.investopedia.com/terms/a/annualized-total-return.asp):
         # Note: this is very approximate given that data are from often non-contiguous segments 
     buys = yReal[yHat>0]
@@ -195,18 +198,18 @@ def calcPerformance(yReal, yHat):
     loss1 = np.sum(np.abs(yReal[np.logical_and(yReal<0, (yHat>buyThreshold))])) 
     profitFactor = win1/loss1
     
-    return [annualizedReturn, returnImprovement, nTrades, percentProfitable, profitFactor, maxDrawdown, rmseImprove]
+    return [annualizedReturn, returnImprovement, nTrades, percentProfitable, profitFactor, maxDrawdown, rmseImprove, rsquared]
     
 # Main function for training models
 def trainModels(x, y, today):
     # Outcome metrics:
     models = [] # tuple:  (mdl, scaler, yTest (unscaled), test_X (scaled))
-    trainResults = np.zeros((nModels,7)) # Annualized Return, vBuy&Hold, # Trades, %Profitable, Profit Factor, Max Drawdown, RMSEimprove
-    devResults = np.zeros((nModels,7))
+    trainResults = np.zeros((nModels,8)) # Annualized Return, vBuy&Hold, # Trades, %Profitable, Profit Factor, Max Drawdown, RMSEimprove
+    devResults = np.zeros((nModels,8))
     
     for ix in range(nModels):
         
-        print("Iteration: ", (ix))
+        print("Training Model: ", (ix))
     
         # Randomly partition data:
         train, dev, test = partitionData(x, y, segmentSize, trainPercent, devPercent)
@@ -214,13 +217,13 @@ def trainModels(x, y, today):
         yTest = yTest.reshape((yTest.shape[0],1))
         
         # Transform the scale of the data to tanh (-1, 1):
-        scaler, trainScaled, devScaled, testScaled = rescaleData(train, dev, test)    
+        scaler, trainScaled, devScaled, testScaled = rescaleData(train, dev, test, scaleColumns)    
         train_X, train_y = separateXY(trainScaled)
         dev_X, dev_y = separateXY(devScaled)
         test_X, test_y = separateXY(testScaled)
         
         # Scale Today's data, for tomorrow's prediction:
-        today_scaled = scaleToday(today, scaler)
+        today_scaled = scaleToday(today, scaler, scaleColumns)
             
         # Fit Model:
         mdl = compileModel(train_X.shape[1])
@@ -249,13 +252,16 @@ def trainModels(x, y, today):
         yhat_today = mdl.predict(today_scaled)
         
         # Invert the scaling:
-        yhat_train = scaler.inverse_transform(np.hstack((train_X, yhat_train)))[:,-1]
+        trainUnscale = np.hstack((train_X, yhat_train))
+        yhat_train = scaler.inverse_transform(trainUnscale[:,scaleColumns])[:,-1]
         yhat_train = yhat_train.reshape((yhat_train.shape[0],1))
         
-        yhat_dev = scaler.inverse_transform(np.hstack((dev_X, yhat_dev)))[:,-1]
+        devUnscale = np.hstack((dev_X, yhat_dev))
+        yhat_dev = scaler.inverse_transform(devUnscale[:,scaleColumns])[:,-1]
         yhat_dev = yhat_dev.reshape((yhat_dev.shape[0],1))
         
-        yhat_today = scaler.inverse_transform(np.hstack((today_scaled, yhat_today)))[:,-1]
+        todayUnscale = np.hstack((today_scaled, yhat_today))
+        yhat_today = scaler.inverse_transform(todayUnscale[:,scaleColumns])[:,-1]
         yhat_today = yhat_today.reshape((yhat_today.shape[0],1))
         
         # Train:  Plot Real vs. Predicted:
@@ -274,15 +280,15 @@ def trainModels(x, y, today):
             # Annualized Return, vBuy&Hold, # Trades, %Profitable, Profit Factor, Max Drawdown, RMSEimprove
             print("Train: Annualized = %.2f%%, vBuy&Hold = %.2f%%, #Trades = %.0f, "
                   "%%Profitable = %.1f%%, Profit Factor = %.2f, Max Drawdown = %.2f%%, "
-                  "RMSE_improvement = %.2f%%" % (trainResults[ix,0], trainResults[ix,1],
+                  "%%RMSE_improve = %.2f%%, R^2 = %.3f" % (trainResults[ix,0], trainResults[ix,1],
                   trainResults[ix,2], trainResults[ix,3], trainResults[ix,4], 
-                  trainResults[ix,5], trainResults[ix,6]))
+                  trainResults[ix,5], trainResults[ix,6], trainResults[ix,7]))
             
             print("Dev: Annualized = %.2f%%, vBuy&Hold = %.2f%%, #Trades = %.0f, "
                   "%%Profitable = %.1f%%, Profit Factor = %.2f, Max Drawdown = %.2f%%, "
-                  "RMSE_improvement = %.2f%%" % (devResults[ix,0], devResults[ix,1],
+                  "%%RMSE_improve = %.2f%%, R^2 = %.3f" % (devResults[ix,0], devResults[ix,1],
                   devResults[ix,2], devResults[ix,3], devResults[ix,4], 
-                  devResults[ix,5], devResults[ix,6]))
+                  devResults[ix,5], devResults[ix,6], devResults[ix,7]))
     
         if verbose==2:
             # Tomorrow's prediction:    
@@ -295,6 +301,7 @@ def saveData(models, trainResults, devResults):
     modelsData = (models, trainResults, devResults)
     pickle.dump(modelsData, f)
     f.close()
+    print('Successfully saved models.')
 
 # Final model data:    
 def saveBest(fname, bestModel, scaler, testResults):
@@ -302,6 +309,74 @@ def saveBest(fname, bestModel, scaler, testResults):
     modelsData = (bestModel, scaler, testResults)
     pickle.dump(modelsData, f2)
     f2.close()
+    print('Successfully saved best model.')
+    
+# AUC of ROC curve:  
+def auc(modelNum, models):
+    bestModel = models[modelNum][0]
+    scaler = models[modelNum][1]
+    yTest = models[modelNum][2] 
+    xTest = models[modelNum][3] 
+    yhat_test = bestModel.predict(xTest)
+    testUnscale = np.hstack((xTest, yhat_test))
+    yhat_test = scaler.inverse_transform(testUnscale[:,scaleColumns])[:,-1]
+    yhat_test = yhat_test.reshape((yhat_test.shape[0],1))   
+        
+    from sklearn.metrics import roc_curve, auc
+    fpr, tpr, thresholds = roc_curve(yTest>0, yhat_test)
+    roc_auc = auc(fpr, tpr)
+    
+    ixOpt = np.argmax(tpr - fpr)
+    optThresh = thresholds[ixOpt]
+    
+    #get_ipython().run_line_magic('matplotlib', 'qt')
+    get_ipython().run_line_magic('matplotlib', 'inline')
+    plt.figure()
+    lw = 2
+    plt.plot(fpr, tpr, color='darkorange',
+             lw=lw, label='ROC curve (area = %0.2f)' % roc_auc)
+    plt.plot([0, 1], [0, 1], color='navy', lw=lw, linestyle='--')
+    plt.xlim([0.0, 1.0])
+    plt.ylim([0.0, 1.05])
+    plt.plot(fpr[ixOpt], tpr[ixOpt], marker='o', color='red')
+    plt.xlabel('False Positive Rate')
+    plt.ylabel('True Positive Rate')
+    plt.title('Receiver operating characteristic')
+    plt.legend(loc="lower right")
+    plt.show()
+    print('Optimal Threshold:  %.4f' % (optThresh))
+
+def testStats(modelNum, models, trainResults, devResults):
+    bestModel = models[modelNum][0]
+    scaler = models[modelNum][1]
+    yTest = models[modelNum][2] 
+    xTest = models[modelNum][3] 
+        
+    print("MODEL %.0f ------------------------------------------------------------------------------------------------------------- MODEL %.0f " % (modelNum, modelNum))
+    # Annualized Return, vBuy&Hold, # Trades, %Profitable, Profit Factor, Max Drawdown, RMSEimprove
+    print("Train: Annualized = %.2f%%, vBuy&Hold = %.2f%%, #Trades = %.0f, "
+          "%%Profitable = %.1f%%, Profit Factor = %.2f, Max Drawdown = %.2f%%, "
+          "%%RMSE_improve = %.2f%%, R^2 = %.3f" % (trainResults[modelNum,0], trainResults[modelNum,1],
+          trainResults[modelNum,2], trainResults[modelNum,3], trainResults[modelNum,4], 
+          trainResults[modelNum,5], trainResults[modelNum,6], trainResults[modelNum,7]))
+    
+    print("Dev: Annualized = %.2f%%, vBuy&Hold = %.2f%%, #Trades = %.0f, "
+          "%%Profitable = %.1f%%, Profit Factor = %.2f, Max Drawdown = %.2f%%, "
+          "%%RMSE_improve = %.2f%%, R^2 = %.3f" % (devResults[modelNum,0], devResults[modelNum,1],
+          devResults[modelNum,2], devResults[modelNum,3], devResults[modelNum,4], 
+          devResults[modelNum,5], devResults[modelNum,6], devResults[modelNum,7]))
+    
+    # Cross-validate final model on Test data:
+    yhat_test = bestModel.predict(xTest)
+    testUnscale = np.hstack((xTest, yhat_test))
+    yhat_test = scaler.inverse_transform(testUnscale[:,scaleColumns])[:,-1]
+    yhat_test = yhat_test.reshape((yhat_test.shape[0],1))   
+    testResults = calcPerformance(np.hstack((xTest,yTest)), yhat_test)    
+    print("Test: Annualized = %.2f%%, vBuy&Hold = %.2f%%, #Trades = %.0f, "
+          "%%Profitable = %.1f%%, Profit Factor = %.2f, Max Drawdown = %.2f%%, "
+          "%%RMSE_improve = %.2f%%, R^2 = %.3f" % (testResults[0], testResults[1],
+          testResults[2], testResults[3], testResults[4], 
+          testResults[5], testResults[6], testResults[7]))
     
 def main():
     # Load data:
@@ -318,34 +393,41 @@ def main():
     models, trainResults, devResults = trainModels(x, y, today)
 
     ###############################################################################
-    # Compare Simulations:  (0) Annualized Return, (1) vBuy&Hold, (2) # Trades, (3) %Profitable, (4) Profit Factor, (5) Max Drawdown, (6) RMSEimprove
-    
-    # Plot annualized returns distribution:
-    plt.hist(devResults[:,0], bins=30, density=True)
-    devResults[:,1].min() 
-    
-    # vBuy&Hold distribution:
-    plt.hist(devResults[:,0], bins=30, density=True)
-    devResults[:,1].min() 
-          
-    # Number of Trades distribution:
-    plt.hist(devResults[:,2], bins=30, density=True)
-    
-    # Percent Profitable distribution:
-    plt.hist(devResults[:,3], bins=30, density=True)
-    
-    # Profit Factor distribution:
-    plt.hist(devResults[:,4], bins=30, density=True)
-    
-    # Max Drawdown distribution:
-    plt.hist(devResults[:,5], bins=30, density=True)
-    
-    # RMSE Improvement distribution:
-    plt.hist(devResults[:,6], bins=30, density=True)
-    np.sum(devResults[:,6]>0)
-    
-    # SELECT BEST MODEL BASED ON METRIC OF CHOICE (default:  RMSE)
-    selectionMetric = 6 # RMSE improvement
+    # Compare Simulations:  (0) Annualized Return, (1) vBuy&Hold, (2) # Trades, 
+        #  (3) %Profitable, (4) Profit Factor, (5) Max Drawdown, (6) RMSEimprove, 
+        # (7) R^2
+    if verbose==2:
+        # Plot annualized returns distribution:
+        plt.title('Annualized Returns')
+        plt.hist(devResults[:,0], bins=30)
+        
+        # vBuy&Hold distribution:
+        plt.title('% Improvement vs. Buy/Hold')
+        plt.hist(devResults[:,1], bins=30)
+              
+        # Number of Trades distribution:
+        plt.title('# Trades')
+        plt.hist(devResults[:,2], bins=30)
+        
+        # Percent Profitable distribution:
+        plt.title('% Profitable')
+        plt.hist(devResults[:,3], bins=30)
+        
+        # Profit Factor distribution:
+        plt.title('Profit Factor')
+        plt.hist(devResults[:,4], bins=30)
+        
+        # Max Drawdown distribution:
+        plt.title('Max Drawdown')
+        plt.hist(devResults[:,5], bins=30)
+        
+        # RMSE Improvement distribution:
+        plt.title('% RMSE Improvement')
+        plt.hist(devResults[:,6], bins=30)
+        
+        # Rsquared distribution:
+        plt.title('Rsquared')
+        plt.hist(devResults[:,7], bins=30)
     
     if selectionMetric==5:
         bestModel = models[devResults[:,selectionMetric].argmin()][0]
@@ -357,22 +439,18 @@ def main():
         scaler = models[devResults[:,selectionMetric].argmax()][1]
         yTest = models[devResults[:,selectionMetric].argmax()][2] 
         xTest = models[devResults[:,selectionMetric].argmax()][3] 
-    
-    #bestModel = models[0][0]
-    #scaler = models[0][1]
-    #yTest = models[0][2] 
-    #xTest = models[0][3] 
         
     # Cross-validate final model on Test data:
     yhat_test = bestModel.predict(xTest)
-    yhat_test = scaler.inverse_transform(np.hstack((xTest, yhat_test)))[:,-1]
+    testUnscale = np.hstack((xTest, yhat_test))
+    yhat_test = scaler.inverse_transform(testUnscale[:,scaleColumns])[:,-1]
     yhat_test = yhat_test.reshape((yhat_test.shape[0],1))   
-    testResults = calcPerformance(np.hstack((xTest,yTest)), yhat_test)
+    testResults = calcPerformance(np.hstack((xTest,yTest)), yhat_test)    
     print("Test: Annualized = %.2f%%, vBuy&Hold = %.2f%%, #Trades = %.0f, "
           "%%Profitable = %.1f%%, Profit Factor = %.2f, Max Drawdown = %.2f%%, "
-          "RMSE_improvement = %.2f%%" % (testResults[0], testResults[1],
+          "%%RMSE_improve = %.2f%%, R^2 = %.3f" % (testResults[0], testResults[1],
           testResults[2], testResults[3], testResults[4], 
-          testResults[5], testResults[6]))
+          testResults[5], testResults[6], testResults[7]))
     
     # Plot test data predictions:
     plotPredictions(np.hstack((xTest,yTest)), yhat_test, 'test_predictions')
@@ -386,19 +464,28 @@ def main():
 # Define Training Parameters:
     
 fpath = '' # path to csv and models files
-csvfile = 'DL_SPX.csv' # csv file containing predictors and outcome
-models_fname = 'dl_models_data.h' # filename to which models data will be stored
-final_fname = 'dl_final_model.h' # filename to which final selected model will be stored
+csvfile = '' # csv file containing predictors and outcome
+models_fname = 'dl_model_01272019.h' # filename to which models data will be stored
+final_fname = 'dl_final_model_01272019.h' # filename to which final selected model will be stored
+
+anyDummyVars = True  # does .csv contain dummy variables?
+if anyDummyVars:
+    nVars = 57 # number of columns in Excel  
+    dummyVars = np.arange(17) # list the indices of dummy var columns in .csv (0-based indexing)
+    scaleColumns =  np.setdiff1d(np.arange(nVars), dummyVars)
 
 # Data Partitions:
 trainPercent = .9 # % of data to use for training set
 devPercent = .05 # % of data to use for dev set
 
 # Model Type Parameters:
-trainingEpochs = 700 # number of training epochs per model
+trainingEpochs = 1000 # number of training epochs per model
 nModels = 10 # number of models to create and optimize for Dev set
-useBestCheckpoint = False # whether to reload last-saved best model for dev prediction
-
+useBestCheckpoint = True # whether to reload last-saved best model for dev prediction
+    
+# SELECT BEST MODEL BASED ON METRIC OF CHOICE (default:  RMSE)
+selectionMetric = 7 # R^2
+        
 lookBack = 1 # lookback period / window size (default: 1, i.e., previous day)
 returnPeriod = 1 # number of days to calculate forward return over (default: 1)
 segmentSize = 10 # used for randomly selecting contiguous data of segmentSize for data partitions (not used for minibatch model training, batch training is default)
@@ -414,9 +501,23 @@ mdlVerbose=0
 if verbose==2:
     mdlVerbose=2
 
-# Uncomment and run main():
-# bestModel, scaler, trainResults, devResults, testResults, models = main()
+#### Uncomment and run main():
+# bestModel, scaler, trainResults, devResults, testResults, models = main();
 
+#### Evaluate Selected Model:
+# modelNum = 5
+# auc(modelNum)
+# testStats(modelNum, models, trainResults, devResults)
+    
+#### Save best model:
+#bestModel = models[modelNum][0]
+#scaler = models[modelNum][1]
+#yTest = models[modelNum][2] 
+#xTest = models[modelNum][3] 
+#    
+#saveData(models, trainResults, devResults) 
+#saveBest(final_fname, bestModel, scaler, testResults)
+    
 #############################################################################
 # To load Model(s):
 #f = open(models_fname, 'rb')
